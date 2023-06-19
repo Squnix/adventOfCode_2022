443cd58848760892ca403970af0f90c227566b2d
Łukasz Kunicki <kunakunicki97@gmail.com>
2023-06-19 21:42:25 +0200

    Project structure update

---
+++ C-C++/src/solution1.c
@@ -0,0 +1,31 @@
+#include "solution1.h"
+
+int getMostCaloriesByElf()
+{
+  // FILE *fileptr = fopen("./data/1.txt", "r");
+  // if (!fileptr)
+  // {
+  //   printf("Failed to open file\n");
+  //   return 1;
+  // };
+  // char *line = NULL;
+  // size_t len = 0;
+  // ssize_t read;
+  // int mostCallories = 0;
+  // int calloriesByElf = 0;
+  // while ((read = getline(&line, &len, fileptr)) != -1)
+  // {
+  //   calloriesByElf += atoi(line);
+  //   if (read == 1)
+  //   {
+  //     mostCallories = mostCallories > calloriesByElf ? mostCallories : calloriesByElf;
+  //     calloriesByElf = 0;
+  //   }
+  // }
+  // fclose(fileptr);
+  // if (line)
+  //   free(line);
+  // printf("Most calories: %d\n", mostCallories)
+  return 1;
+  // return mostCallories;
+}
